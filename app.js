dd8c58c9409b824448e0a711c652e80a04f59270
Samuel Borges <samuelvborgess@gmail.com>
2025-01-29 15:24:40 -0400

    Adiciona logica e funcionalidades com javascript

--- app.js
+++ app.js
@@ -1,1 +1,42 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigos = []
+let meusAmigos = document.getElementById('listaAmigos');
+let resultado = document.querySelector('p');
+
+
+function Limpar (){
+    meusAmigos.innerHTML = "";
+    resultado.innerHTML = "";
+    amigos = []
+}
+function atualizarLista() {
+    let amigosDaLista = "";
+    amigos.forEach(function (amigo) {
+        amigosDaLista += `<li>${amigo}</li>`; 
+    });
+    meusAmigos.innerHTML = amigosDaLista;
+}
+
+function adicionarAmigo(){
+    let valor = document.getElementById('amigo');
+    
+    if(valor.value === ""){
+        alert('Digite um nome no campo em branco')
+    } else {
+    amigos.push(valor.value); 
+    valor.value = "";
+    atualizarLista()
+    console.log(amigos); 
+    }
+};
+
+function sortearAmigo(){
+    
+    if (amigos.length > 0){
+        let sorteio = Math.floor(Math.random() * amigos.length);
+        
+        resultado.innerHTML = `Amigo sorteado: ${amigos[sorteio]}!`;
+    } else {
+        alert('adicione o nome de seus amigos na lista')
+    }
+};
+    
---
+++ readme.md
@@ -0,0 +1,17 @@
+# Amigo Secreto
+
+Este é um projeto simples de sorteio de **Amigo Secreto** em JavaScript. Ele permite que você adicione os nomes de seus amigos, sorteie um amigo secreto aleatoriamente e visualize os resultados diretamente no navegador.
+
+## Funcionalidades
+
+- **Adicionar amigos**: Insira os nomes de seus amigos na lista.
+- **Sortear amigo**: Ao clicar no botão de sorteio, o projeto escolhe aleatoriamente um amigo secreto da lista.
+- **Reset**: É um botão para **limpar a lista de amigos** e reiniciar o processo de sorteio.
+
+## Tecnologias Utilizadas
+
+- **HTML**: Estrutura da página.
+- **CSS**: Estilos para deixar a interface mais bonita e interativa.
+- **JavaScript**: Lógica de adicionar amigos à lista e realizar o sorteio.
+  
+
